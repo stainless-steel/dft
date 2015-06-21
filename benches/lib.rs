7666242d8d8994ca0c4a795f966caa916588bc81
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-06-21 16:29:44 -0400

    Add several benchmarks

---
+++ benches/lib.rs
@@ -0,0 +1,18 @@
+#![feature(test)]
+
+extern crate fft;
+extern crate test;
+
+#[bench] fn forward_0004(bencher: &mut test::Bencher) { forward(   4, bencher); }
+#[bench] fn forward_0016(bencher: &mut test::Bencher) { forward(  16, bencher); }
+#[bench] fn forward_0064(bencher: &mut test::Bencher) { forward(  64, bencher); }
+#[bench] fn forward_0256(bencher: &mut test::Bencher) { forward( 256, bencher); }
+#[bench] fn forward_1024(bencher: &mut test::Bencher) { forward(1024, bencher); }
+#[bench] fn forward_4096(bencher: &mut test::Bencher) { forward(4096, bencher); }
+
+fn forward(size: usize, bencher: &mut test::Bencher) {
+    let mut data = vec![42.0; 2 * size];
+    bencher.iter(|| {
+        test::black_box(fft::forward(&mut data));
+    });
+}
