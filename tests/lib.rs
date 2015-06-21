3309618c10fc60ec2899826776c415141c4b20cb
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-06-21 16:33:39 -0400

    Increase the comparison precision in the tests

--- tests/lib.rs
+++ tests/lib.rs
@@ -7,12 +7,12 @@ mod fixtures;
 fn forward() {
     let mut data = fixtures::TIME_DATA.to_vec();
     fft::forward(&mut data);
-    assert::close(&data, &fixtures::FREQUENCY_DATA[..], 1e-13);
+    assert::close(&data, &fixtures::FREQUENCY_DATA[..], 1e-14);
 }
 
 #[test]
 fn inverse() {
     let mut data = fixtures::FREQUENCY_DATA.to_vec();
     fft::inverse(&mut data, true);
-    assert::close(&data, &fixtures::TIME_DATA[..], 1e-13);
+    assert::close(&data, &fixtures::TIME_DATA[..], 1e-14);
 }
