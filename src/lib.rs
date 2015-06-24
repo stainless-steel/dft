b43cce29d06ace8adc5b2a909f20e1b7f73d1437
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-06-24 17:16:48 -0400

    Add the forward FFT for real data

--- src/lib.rs
+++ src/lib.rs
@@ -27,6 +27,63 @@ pub fn forward(data: &mut [c64]) {
     perform(data, n, false);
 }
 
+/// Perform the forward transform of real data.
+///
+/// The number of points should be a power of two. The data are replaced by the
+/// positive frequency half of their complex Fourier transform. The real-valued
+/// first and last components of the complex transform are returned as elements
+/// data[0] and data[1], respectively.
+///
+/// ## References
+///
+/// 1. William H. Press, Saul A. Teukolsky, William T. Vetterling, Brian P.
+///    Flannery, “Numerical Recipes 3rd Edition: The Art of Scientific
+///    Computing,” Cambridge University Press, 2007.
+pub fn forward_real(data: &mut [f64]) {
+    use std::f64::consts::PI;
+    use std::slice::from_raw_parts_mut;
+
+    let n = data.len();
+    power_of_two!(n);
+
+    forward(unsafe {
+        from_raw_parts_mut(data.as_mut_ptr() as *mut _, n / 2)
+    });
+
+    let (c1, c2) = (0.5, -0.5);
+    let (wpr, wpi) = {
+        let theta = -PI / (n / 2) as f64;
+        let temp = (0.5 * theta).sin();
+        (-2.0 * temp * temp, theta.sin())
+    };
+    let mut wr = 1.0 + wpr;
+    let mut wi = wpi;
+    for i in 1..(n / 2 / 2) {
+        let i1 = i + i;
+        let i2 = 1 + i1;
+        let i3 = n - i1;
+        let i4 = 1 + i3;
+
+        let h1r =  c1 * (data[i1] + data[i3]);
+        let h1i =  c1 * (data[i2] - data[i4]);
+        let h2r = -c2 * (data[i2] + data[i4]);
+        let h2i =  c2 * (data[i1] - data[i3]);
+
+        data[i1] =  h1r + wr * h2r - wi * h2i;
+        data[i2] =  h1i + wr * h2i + wi * h2r;
+        data[i3] =  h1r - wr * h2r + wi * h2i;
+        data[i4] = -h1i + wr * h2i + wi * h2r;
+
+        let temp = wr;
+        wr = wr * wpr - wi * wpi + wr;
+        wi = wi * wpr + temp * wpi + wi;
+    }
+    data[n / 2 + 1] *= -1.0;
+    let temp = data[0];
+    data[0] = temp + data[1];
+    data[1] = temp - data[1];
+}
+
 /// Perform the inverse transform.
 ///
 /// The number of points should be a power of two.
@@ -40,7 +97,26 @@ pub fn inverse(data: &mut [c64], scaling: bool) {
     }
 }
 
-#[inline(always)]
+/// Expand a compressed representation produced by `forward_real`.
+pub fn unpack_real(data: &[f64]) -> Vec<c64> {
+    let n = data.len();
+    power_of_two!(n);
+
+    let mut cdata = Vec::with_capacity(n);
+    unsafe { cdata.set_len(n) };
+
+    cdata[0] = c64(data[0], 0.0);
+    for i in 1..(n / 2) {
+        cdata[i] = c64(data[2 * i], data[2 * i + 1]);
+    }
+    cdata[n / 2] = c64(data[1], 0.0);
+    for i in (n / 2 + 1)..n {
+        cdata[i] = c64(data[2 * (n - i)], -data[2 * (n - i) + 1]);
+    }
+
+    cdata
+}
+
 fn rearrange(data: &mut [c64], n: usize) {
     let mut target = 0;
     for position in 0..n {
@@ -56,7 +132,6 @@ fn rearrange(data: &mut [c64], n: usize) {
     }
 }
 
-#[inline(always)]
 fn perform(data: &mut [c64], n: usize, inverse: bool) {
     use std::f64::consts::PI;
 
@@ -83,10 +158,28 @@ fn perform(data: &mut [c64], n: usize, inverse: bool) {
     }
 }
 
-#[inline(always)]
 fn scale(data: &mut [c64], n: usize) {
     let factor = 1.0 / n as f64;
     for position in 0..n {
         data[position] = data[position] * factor;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use c64;
+
+    #[test]
+    fn unpack_real() {
+        let data = (0..4).map(|i| (i + 1) as f64).collect::<Vec<_>>();
+        assert_eq!(::unpack_real(&data), vec![
+            c64(1.0, 0.0), c64(3.0, 4.0), c64(2.0, 0.0), c64(3.0, -4.0),
+        ]);
+
+        let data = (0..8).map(|i| (i + 1) as f64).collect::<Vec<_>>();
+        assert_eq!(::unpack_real(&data), vec![
+            c64(1.0, 0.0), c64(3.0, 4.0), c64(5.0, 6.0), c64(7.0, 8.0),
+            c64(2.0, 0.0), c64(7.0, -8.0), c64(5.0, -6.0), c64(3.0, -4.0),
+        ]);
     }
 }
--- tests/lib.rs
+++ tests/lib.rs
@@ -6,21 +6,44 @@ mod fixtures;
 #[test]
 fn forward() {
     let mut data = fixtures::TIME_DATA.to_vec();
-    fft::forward(reinterpret(&mut data));
-    assert::close(&data, &fixtures::FREQUENCY_DATA[..], 1e-14);
+    fft::forward(as_c64_mut(&mut data));
+    assert::close(&data, &fixtures::FREQUENCY_DATA_FROM_COMPLEX[..], 1e-14);
+}
+
+#[test]
+fn forward_real() {
+    let mut data = fixtures::TIME_DATA.to_vec();
+    {
+        let mut data = to_c64(&data);
+        fft::forward(&mut data);
+        assert::close(as_f64(&data), &fixtures::FREQUENCY_DATA_FROM_REAL[..], 1e-13);
+    }
+    {
+        fft::forward_real(&mut data);
+        let data = fft::unpack_real(&data);
+        assert::close(as_f64(&data), &fixtures::FREQUENCY_DATA_FROM_REAL[..], 1e-13);
+    }
 }
 
 #[test]
 fn inverse() {
-    let mut data = fixtures::FREQUENCY_DATA.to_vec();
-    fft::inverse(reinterpret(&mut data), true);
+    let mut data = fixtures::FREQUENCY_DATA_FROM_COMPLEX.to_vec();
+    fft::inverse(as_c64_mut(&mut data), true);
     assert::close(&data, &fixtures::TIME_DATA[..], 1e-14);
 }
 
-fn reinterpret<'l>(slice: &'l mut [f64]) -> &'l mut [fft::c64] {
+fn as_f64<'l>(slice: &'l [fft::c64]) -> &'l [f64] {
     unsafe {
-        let length = slice.len();
-        assert!(length % 2 == 0, "the number of elements should be even");
-        std::slice::from_raw_parts_mut(slice.as_mut_ptr() as *mut _, length / 2)
+        std::slice::from_raw_parts(slice.as_ptr() as *const _, 2 * slice.len())
     }
+}
+
+fn as_c64_mut<'l>(slice: &'l mut [f64]) -> &'l mut [fft::c64] {
+    unsafe {
+        std::slice::from_raw_parts_mut(slice.as_mut_ptr() as *mut _, slice.len() / 2)
+    }
+}
+
+fn to_c64(slice: &[f64]) -> Vec<fft::c64> {
+    slice.iter().map(|&re| fft::c64(re, 0.0)).collect()
 }
