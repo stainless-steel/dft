1836e66d08f4b736202ecdf1d1157d6b24b84509
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-06-25 16:18:22 -0400

    Add benchmarks for the real part

--- benches/lib.rs
+++ benches/lib.rs
@@ -14,7 +14,19 @@ use test::{Bencher, black_box};
 #[bench] fn complex_forward_1024(bencher: &mut Bencher) { complex_forward(1024, bencher); }
 #[bench] fn complex_forward_4096(bencher: &mut Bencher) { complex_forward(4096, bencher); }
 
+#[bench] fn real_forward_0004(bencher: &mut Bencher) { real_forward(   4, bencher); }
+#[bench] fn real_forward_0016(bencher: &mut Bencher) { real_forward(  16, bencher); }
+#[bench] fn real_forward_0064(bencher: &mut Bencher) { real_forward(  64, bencher); }
+#[bench] fn real_forward_0256(bencher: &mut Bencher) { real_forward( 256, bencher); }
+#[bench] fn real_forward_1024(bencher: &mut Bencher) { real_forward(1024, bencher); }
+#[bench] fn real_forward_4096(bencher: &mut Bencher) { real_forward(4096, bencher); }
+
 fn complex_forward(size: usize, bencher: &mut Bencher) {
-    let mut data = vec![c64(42.0, 0.0); size];
+    let mut data = vec![c64(42.0, 69.0); size];
     bencher.iter(|| black_box(fft::complex::forward(&mut data)));
+}
+
+fn real_forward(size: usize, bencher: &mut Bencher) {
+    let mut data = vec![42.0; 2 * size];
+    bencher.iter(|| black_box(fft::real::forward(&mut data)));
 }
