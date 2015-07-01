7435c6153d399ab53a84e8d2f326b4bcffbc21bb
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-07-01 08:07:28 -0400

    Make use of is_power_of_two

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,7 @@ extern crate complex as number;
 macro_rules! power_of_two(
     ($data:expr) => ({
         let n = $data.len();
-        if n < 1 || n & (n - 1) != 0 {
+        if !n.is_power_of_two() {
             panic!("expected the number of points to be a power of two");
         }
         n
