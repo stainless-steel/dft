b1a133b45ed5b3cb24ca06598146fa252cb9852d
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-11-17 08:13:44 +0100

    Add a pair of 8192 benchmarks

--- benches/lib.rs
+++ benches/lib.rs
@@ -6,27 +6,29 @@ extern crate test;
 use dft::{Operation, Plan, Transform, c64};
 use test::{Bencher, black_box};
 
-#[bench] fn complex_transform_0004(bencher: &mut Bencher) { complex_transform(   4, bencher); }
-#[bench] fn complex_transform_0016(bencher: &mut Bencher) { complex_transform(  16, bencher); }
-#[bench] fn complex_transform_0064(bencher: &mut Bencher) { complex_transform(  64, bencher); }
-#[bench] fn complex_transform_0256(bencher: &mut Bencher) { complex_transform( 256, bencher); }
-#[bench] fn complex_transform_1024(bencher: &mut Bencher) { complex_transform(1024, bencher); }
-#[bench] fn complex_transform_4096(bencher: &mut Bencher) { complex_transform(4096, bencher); }
+#[bench] fn complex_0004(bencher: &mut Bencher) { complex(   4, bencher); }
+#[bench] fn complex_0016(bencher: &mut Bencher) { complex(  16, bencher); }
+#[bench] fn complex_0064(bencher: &mut Bencher) { complex(  64, bencher); }
+#[bench] fn complex_0256(bencher: &mut Bencher) { complex( 256, bencher); }
+#[bench] fn complex_1024(bencher: &mut Bencher) { complex(1024, bencher); }
+#[bench] fn complex_4096(bencher: &mut Bencher) { complex(4096, bencher); }
+#[bench] fn complex_8192(bencher: &mut Bencher) { complex(8192, bencher); }
 
-#[bench] fn real_transform_0004(bencher: &mut Bencher) { real_transform(   4, bencher); }
-#[bench] fn real_transform_0016(bencher: &mut Bencher) { real_transform(  16, bencher); }
-#[bench] fn real_transform_0064(bencher: &mut Bencher) { real_transform(  64, bencher); }
-#[bench] fn real_transform_0256(bencher: &mut Bencher) { real_transform( 256, bencher); }
-#[bench] fn real_transform_1024(bencher: &mut Bencher) { real_transform(1024, bencher); }
-#[bench] fn real_transform_4096(bencher: &mut Bencher) { real_transform(4096, bencher); }
+#[bench] fn real_0004(bencher: &mut Bencher) { real(   4, bencher); }
+#[bench] fn real_0016(bencher: &mut Bencher) { real(  16, bencher); }
+#[bench] fn real_0064(bencher: &mut Bencher) { real(  64, bencher); }
+#[bench] fn real_0256(bencher: &mut Bencher) { real( 256, bencher); }
+#[bench] fn real_1024(bencher: &mut Bencher) { real(1024, bencher); }
+#[bench] fn real_4096(bencher: &mut Bencher) { real(4096, bencher); }
+#[bench] fn real_8192(bencher: &mut Bencher) { real(8192, bencher); }
 
-fn complex_transform(size: usize, bencher: &mut Bencher) {
+fn complex(size: usize, bencher: &mut Bencher) {
     let mut data = vec![c64::new(42.0, 69.0); size];
     let plan = Plan::new(Operation::Forward, size);
     bencher.iter(|| black_box(data.transform(&plan)));
 }
 
-fn real_transform(size: usize, bencher: &mut Bencher) {
+fn real(size: usize, bencher: &mut Bencher) {
     let mut data = vec![42.0; 2 * size];
     let plan = Plan::new(Operation::Forward, 2 * size);
     bencher.iter(|| black_box(data.transform(&plan)));
