605d5ecd95d8772a90a1beac848249f7d99fe2bf
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-08-08 10:52:27 +0200

    Adjust the tests

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,64 +1,64 @@
 extern crate assert;
 extern crate dft;
 
-use dft::{Operation, Plan, c64};
+use dft::{Operation, Plan, c64, transform, unpack};
 
 mod fixtures;
 
 #[test]
 fn complex_forward_1() {
     let mut data = vec![c64::new(1.0, -2.0)];
-    dft::transform(&mut data, &Plan::new(Operation::Forward, 1));
+    transform(&mut data, &Plan::new(Operation::Forward, 1));
     assert_eq!(data, vec![c64::new(1.0, -2.0)]);
 }
 
 #[test]
 fn complex_forward_2() {
     let mut data = vec![c64::new(1.0, -2.0), c64::new(3.0, -4.0)];
-    dft::transform(&mut data, &Plan::new(Operation::Forward, 2));
+    transform(&mut data, &Plan::new(Operation::Forward, 2));
     assert_eq!(data, vec![c64::new(4.0, -6.0), c64::new(-2.0, 2.0)]);
 }
 
 #[test]
 fn complex_forward_128() {
     let mut data = fixtures::TIME_DATA_256.to_vec();
-    dft::transform(as_c64_mut(&mut data), &Plan::new(Operation::Forward, 128));
+    transform(as_c64_mut(&mut data), &Plan::new(Operation::Forward, 128));
     assert::close(&data, &fixtures::FREQUENCY_DATA_128_COMPLEX[..], 1e-14);
 }
 
 #[test]
 fn complex_forward_real_256() {
     let mut data = to_c64(&fixtures::TIME_DATA_256);
-    dft::transform(&mut data, &Plan::new(Operation::Forward, 256));
+    transform(&mut data, &Plan::new(Operation::Forward, 256));
     assert::close(as_f64(&data), &fixtures::FREQUENCY_DATA_256_REAL_UNPACKED[..], 1e-13);
 }
 
 #[test]
 fn complex_inverse_128() {
     let mut data = fixtures::FREQUENCY_DATA_128_COMPLEX.to_vec();
-    dft::transform(as_c64_mut(&mut data), &Plan::new(Operation::Inverse, 128));
+    transform(as_c64_mut(&mut data), &Plan::new(Operation::Inverse, 128));
     assert::close(&data, &fixtures::TIME_DATA_256[..], 1e-14);
 }
 
 #[test]
 fn real_forward_1() {
     let mut data = vec![1.0];
-    dft::transform(&mut data, &Plan::new(Operation::Forward, 1));
-    assert_eq!(dft::unpack(&data), vec![c64::new(1.0, 0.0)]);
+    transform(&mut data, &Plan::new(Operation::Forward, 1));
+    assert_eq!(unpack(&data), vec![c64::new(1.0, 0.0)]);
 }
 
 #[test]
 fn real_forward_2() {
     let mut data = vec![1.0, -2.0];
-    dft::transform(&mut data, &Plan::new(Operation::Forward, 2));
-    assert_eq!(dft::unpack(&data), vec![c64::new(-1.0, 0.0), c64::new(3.0, 0.0)]);
+    transform(&mut data, &Plan::new(Operation::Forward, 2));
+    assert_eq!(unpack(&data), vec![c64::new(-1.0, 0.0), c64::new(3.0, 0.0)]);
 }
 
 #[test]
 fn real_forward_4() {
     let mut data = vec![1.0, -2.0, 3.0, -4.0];
-    dft::transform(&mut data, &Plan::new(Operation::Forward, 4));
-    assert_eq!(dft::unpack(&data), vec![
+    transform(&mut data, &Plan::new(Operation::Forward, 4));
+    assert_eq!(unpack(&data), vec![
        c64::new(-2.0, 0.0), c64::new(-2.0, -2.0), c64::new(10.0, 0.0), c64::new(-2.0, 2.0),
     ]);
 }
@@ -66,32 +66,32 @@ fn real_forward_4() {
 #[test]
 fn real_forward_256() {
     let mut data = fixtures::TIME_DATA_256.to_vec();
-    dft::transform(&mut data, &Plan::new(Operation::Forward, 256));
+    transform(&mut data, &Plan::new(Operation::Forward, 256));
     assert::close(&data, &fixtures::FREQUENCY_DATA_256_REAL_PACKED[..], 1e-13);
-    let data = dft::unpack(&data);
+    let data = unpack(&data);
     assert::close(as_f64(&data), &fixtures::FREQUENCY_DATA_256_REAL_UNPACKED[..], 1e-13);
 }
 
 #[test]
 fn real_forward_512() {
     let mut data = fixtures::TIME_DATA_512.to_vec();
-    dft::transform(&mut data, &Plan::new(Operation::Forward, 512));
-    let data = dft::unpack(&data);
+    transform(&mut data, &Plan::new(Operation::Forward, 512));
+    let data = unpack(&data);
     assert::close(as_f64(&data), &fixtures::FREQUENCY_DATA_512_REAL_UNPACKED[..], 1e-12);
 }
 
 #[test]
 fn real_inverse_256() {
     let mut data = fixtures::FREQUENCY_DATA_256_REAL_PACKED.to_vec();
-    dft::transform(&mut data, &Plan::new(Operation::Inverse, 256));
+    transform(&mut data, &Plan::new(Operation::Inverse, 256));
     assert::close(&data, &fixtures::TIME_DATA_256[..], 1e-14);
 }
 
 #[test]
 fn real_inverse_512() {
     let mut data = fixtures::TIME_DATA_512.to_vec();
-    dft::transform(&mut data, &Plan::new(Operation::Forward, 512));
-    dft::transform(&mut data, &Plan::new(Operation::Inverse, 512));
+    transform(&mut data, &Plan::new(Operation::Forward, 512));
+    transform(&mut data, &Plan::new(Operation::Inverse, 512));
     assert::close(&data, &fixtures::TIME_DATA_512[..], 1e-14);
 }
 
